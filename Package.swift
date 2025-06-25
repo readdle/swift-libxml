d1b83d6ce7819a349c3b7279b6ffa0bd08266ea8
Yaroslav Biletskyi <ybiletskyi@readdle.com>
2025-06-25 13:06:48 +0300

    Dev: add unix support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
-let package = Package(name: "libxml2")
+let package = Package(
+    name: "libxml",
+    products: [
+        .library(name: "libxml", targets: ["libxml"]),
+    ],
+    targets: [
+        .systemLibrary(
+            name: "libxml",
+            pkgConfig: "libxml-2.0",
+            providers: [
+                .apt(["libxml2-dev"]),
+            ]
+        )
+    ]
+)
